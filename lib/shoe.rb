68df49f2b86939530e2e40c31026b54cc8356e83
Donna Torres <todonnatorres@icloud.com>
2020-06-08 19:13:23 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,14 +6,14 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    Brands << @brand
+    if !(BRANDS.include?(@brand))
+      BRANDS << @brand
     end
   end
 
-  def initialize(brand)
-    @brand = brand
-    if !(BRANDS.include?(@brand))
-      BRANDS << brand
-    end
+  def cobble
+    self.condition = "new"
+    puts "Your shoe is as good as new!"
+  end
 
 end
